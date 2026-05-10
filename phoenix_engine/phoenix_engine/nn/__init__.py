70ed234e840b4a8a66ce84680633ba4f459f92f0
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 23:52:28 +0300

    Added Embedding import from embeddings module

--- phoenix_engine/phoenix_engine/nn/__init__.py
+++ phoenix_engine/phoenix_engine/nn/__init__.py
@@ -1,7 +1,8 @@
 from .module import Module, Parameter
 from .linear import Linear
 from .loss import MSELoss
-from .normalization import LayerNorm, Embedding
+from .normalization import LayerNorm
+from .embeddings import Embedding
 from .attention import MultiHeadAttention
 
 __all__ = ["Module", "Parameter", "Linear", "MSELoss", "LayerNorm", "Embedding", "MultiHeadAttention"]
