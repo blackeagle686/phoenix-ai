39e50facbf44c12f68e54798d50d39eb36c0087c
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 19:13:53 +0300

    Added TensorData and enum bindings to Python module

--- phoenix_engine/csrc/bindings.cpp
+++ phoenix_engine/csrc/bindings.cpp
@@ -1,7 +1,10 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <string>
+#include "core/tensor_data.h"
 
 namespace py = pybind11;
+using namespace phoenix;
 
 std::string hello() {
     return "Hello from Phoenix C++ Engine!";
@@ -15,4 +18,26 @@ PYBIND11_MODULE(_phoenix_backend, m) {
     m.doc() = "Phoenix Engine Low-Level C++ Backend";
     m.def("hello", &hello, "A function that returns a hello string");
     m.def("add", &add, "A function that adds two numbers");
+
+    py::enum_<Device>(m, "Device")
+        .value("CPU", Device::CPU)
+        .value("CUDA", Device::CUDA)
+        .export_values();
+
+    py::enum_<DType>(m, "DType")
+        .value("Float32", DType::Float32)
+        .value("Float64", DType::Float64)
+        .value("Int32", DType::Int32)
+        .value("Int64", DType::Int64)
+        .export_values();
+
+    py::class_<TensorData, std::shared_ptr<TensorData>>(m, "TensorData")
+        .def(py::init<const std::vector<size_t>&, DType, Device>(), 
+             py::arg("shape"), py::arg("dtype"), py::arg("device") = Device::CPU)
+        .def("shape", &TensorData::shape)
+        .def("dtype", &TensorData::dtype)
+        .def("device", &TensorData::device)
+        .def("size", &TensorData::size)
+        .def("num_bytes", &TensorData::num_bytes)
+        .def("__repr__", &TensorData::to_string);
 }
