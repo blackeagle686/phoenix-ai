0f22465d643bda2d68211aadd2c8a7599543bd06
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 22:04:28 +0300

    Add Linear layer and Module base class

---
+++ phoenix_engine/python/nn/linear.py
@@ -0,0 +1,27 @@
+from .module import Module, Parameter
+from ..tensor import Tensor
+import math
+
+class Linear(Module):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        
+        # Initialize weights using randn (simple initialization for now)
+        # In a real framework, we'd use Xavier or He initialization
+        self.weight = Parameter(Tensor.randn([in_features, out_features]).data)
+        
+        if bias:
+            self.bias = Parameter(Tensor.randn([1, out_features]).data)
+        else:
+            self.bias = None
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = x.matmul(self.weight)
+        if self.bias is not None:
+            out = out + self.bias
+        return out
+
+    def __repr__(self):
+        return f"Linear(in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None})"
---
+++ phoenix_engine/python/nn/module.py
@@ -0,0 +1,39 @@
+from ..tensor import Tensor
+from typing import List, Dict, Any
+
+class Parameter(Tensor):
+    def __init__(self, data, requires_grad=True):
+        super().__init__(data, requires_grad=requires_grad)
+
+class Module:
+    def __init__(self):
+        self._parameters: Dict[str, Parameter] = {}
+        self._modules: Dict[str, 'Module'] = {}
+        self.training = True
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if isinstance(value, Parameter):
+            self._parameters[name] = value
+        elif isinstance(value, Module):
+            self._modules[name] = value
+        super().__setattr__(name, value)
+
+    def parameters(self) -> List[Parameter]:
+        params = list(self._parameters.values())
+        for module in self._modules.values():
+            params.extend(module.parameters())
+        return params
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def train(self, mode: bool = True):
+        self.training = mode
+        for module in self._modules.values():
+            module.train(mode)
+
+    def eval(self):
+        self.train(False)
