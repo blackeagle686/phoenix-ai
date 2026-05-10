05cc9493dc6cb18d4b5424c4a0a2f4504db15a99
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 22:51:37 +0300

    Added LayerNorm and Embedding to nn module

--- phoenix_engine/phoenix_engine/nn/__init__.py
+++ phoenix_engine/phoenix_engine/nn/__init__.py
@@ -1,5 +1,6 @@
 from .module import Module, Parameter
 from .linear import Linear
 from .loss import MSELoss
+from .normalization import LayerNorm, Embedding
 
 __all__ = ["Module", "Parameter", "Linear", "MSELoss"]
