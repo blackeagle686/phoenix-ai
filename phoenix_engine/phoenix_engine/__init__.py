fff179bd10ae105f1f89045a1234702e39f12310
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 23:17:53 +0300

    Added nn and optim modules to __all__

--- phoenix_engine/phoenix_engine/__init__.py
+++ phoenix_engine/phoenix_engine/__init__.py
@@ -1,4 +1,6 @@
 from _phoenix_backend import hello, add
 from .tensor import Tensor
+from . import nn
+from . import optim
 
-__all__ = ["hello", "add", "Tensor"]
+__all__ = ["hello", "add", "Tensor", "nn", "optim"]
