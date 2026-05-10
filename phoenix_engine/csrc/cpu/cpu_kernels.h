655f30f87f97ebbc46ef702d4029e92d586fb9ee
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 23:55:48 +0300

    Add softmax_cross_entropy_backward function declaration

--- phoenix_engine/csrc/cpu/cpu_kernels.h
+++ phoenix_engine/csrc/cpu/cpu_kernels.h
@@ -22,6 +22,7 @@ TensorDataPtr embedding_forward(const TensorDataPtr& indices, const TensorDataPt
 TensorDataPtr masked_fill(const TensorDataPtr& a, const TensorDataPtr& mask, float value);
 TensorDataPtr tril(const std::vector<size_t>& shape);
 TensorDataPtr softmax_cross_entropy(const TensorDataPtr& logits, const TensorDataPtr& targets);
+TensorDataPtr softmax_cross_entropy_backward(const TensorDataPtr& grad_out, const TensorDataPtr& logits, const TensorDataPtr& targets);
 
 // General Matrix Multiply (2D)
 TensorDataPtr gemm(const TensorDataPtr& a, const TensorDataPtr& b);
