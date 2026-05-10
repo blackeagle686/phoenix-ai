6ae8ea7dcd376d090352a953487eca876e30b657
Mohamed Alaa <mathematecs1@gmail.com>
2026-05-10 23:52:18 +0300

    Refactored LayerNorm initialization to use ones/zeros

--- phoenix_engine/phoenix_engine/nn/normalization.py
+++ phoenix_engine/phoenix_engine/nn/normalization.py
@@ -10,36 +10,8 @@ class LayerNorm(Module):
         self.eps = eps
         
         # Initialize weight to 1 and bias to 0
-        w_data = Tensor.randn(normalized_shape).data
-        b_data = Tensor.randn(normalized_shape).data
-        
-        # We need a fill operation, but for now we can just use randn and set requires_grad=True
-        # In a real engine, we'd use ones() and zeros()
-        self.weight = Parameter(w_data)
-        self.bias = Parameter(b_data)
+        self.weight = Parameter(Tensor.ones(normalized_shape))
+        self.bias = Parameter(Tensor.zeros(normalized_shape))
         
     def forward(self, x: Tensor) -> Tensor:
         return x.layernorm(self.weight, self.bias, self.eps)
-
-class Embedding(Module):
-    def __init__(self, num_embeddings: int, embedding_dim: int):
-        super().__init__()
-        self.num_embeddings = num_embeddings
-        self.embedding_dim = embedding_dim
-        
-        # Standard normal initialization
-        w_data = Tensor.randn(num_embeddings, embedding_dim).data
-        self.weight = Parameter(w_data)
-        
-    def forward(self, indices: Tensor) -> Tensor:
-        import _phoenix_backend as pb
-        out_data = pb.embedding(self.weight.data, indices.data)
-        out = Tensor(out_data, _prev=(self.weight, indices))
-        
-        def _backward():
-            if self.weight.requires_grad:
-                # Sparse update would go here
-                pass
-                
-        out._backward = _backward
-        return out
