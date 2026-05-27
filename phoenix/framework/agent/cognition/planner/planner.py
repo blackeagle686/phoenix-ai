a584b97d97b6e7bdac592f3f2b03d8ceb9b9e95d
blackeagle686 <mathematecs1@gmail.com>
2026-05-27 11:12:43 +0300

    Updated UUID import to uuid4

--- phoenix/framework/agent/cognition/planner/planner.py
+++ phoenix/framework/agent/cognition/planner/planner.py
@@ -5,7 +5,8 @@ from .base import BasePlanner
 from ..utils import parse_llm_json
 from phoenix.services.cache import RedisCache
 from phoenix.framework.agent.cognition.planner.schema import * 
-from uuid import UUID
+from uuid import uuid4
+
 async def get_redis_client():
     client = RedisCache()
     await client.init()
