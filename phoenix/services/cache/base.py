f0b1e5db0b63f0ae46e7d204d4dcc6a61f789219
blackeagle686 <mathematecs1@gmail.com>
2026-05-27 11:23:27 +0300

    Add keys and get_all methods to BaseCache, make ttl optional in set

--- phoenix/services/cache/base.py
+++ phoenix/services/cache/base.py
@@ -5,8 +5,14 @@ class BaseCache(BaseService):
     async def get(self, key: str) -> Optional[Any]:
         raise NotImplementedError
 
-    async def set(self, key: str, value: Any, ttl: int) -> None:
+    async def set(self, key: str, value: Any, ttl: Optional[int] = None) -> None:
         raise NotImplementedError
 
     async def delete(self, key: str) -> None:
+        raise NotImplementedError
+
+    async def keys(self, pattern: str = "*") -> List[str]:
+        raise NotImplementedError
+
+    async def get_all(self, pattern: str = "*") -> Dict[str, Any]:
         raise NotImplementedError
