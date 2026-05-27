08b52f2aad0cda8f99f4c4eecef20e4f039dfc8e
blackeagle686 <mathematecs1@gmail.com>
2026-05-27 13:21:29 +0300

    Bump version to 0.2.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="phx-ashborn",
-    version="0.2.7",
+    version="0.2.8",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=1.0.0",
