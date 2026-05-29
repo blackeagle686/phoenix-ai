c57f56dd782102e022e40607062ea5b558f2a134
blackeagle686 <mathematecs1@gmail.com>
2026-05-29 21:55:10 +0300

    Auto-commit: 2026-05-29 21:55:10

--- phoenix/framework/agent/tools/bank/__init__.py
+++ phoenix/framework/agent/tools/bank/__init__.py
@@ -1,5 +1,9 @@
 # Phoenix Tool Bank
 from .web import WebScraperTool, WikipediaSearchTool, ArxivSearchTool
 from .devops import GitHubTool
+from .productivity import SlackTool, EmailTool
 
-__all__ = ["WebScraperTool", "WikipediaSearchTool", "ArxivSearchTool", "GitHubTool"]
+__all__ = [
+    "WebScraperTool", "WikipediaSearchTool", "ArxivSearchTool", 
+    "GitHubTool", "SlackTool", "EmailTool"
+]
