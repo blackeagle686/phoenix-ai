496672bf7473315d674cdf4b215372a4074a489f
blackeagle686 <mathematecs1@gmail.com>
2026-05-30 09:48:45 +0300

    Auto-commit: 2026-05-30 09:48:45

--- phoenix/services/cache/redis_cache.py
+++ phoenix/services/cache/redis_cache.py
@@ -38,7 +38,7 @@ class RedisCache(BaseCache):
         if not self.redis:
             await self.init()
             if self._failed: return None
-            
+            hello/1680
         try:
             value = await self.redis.get(key)
             if value:
