c20d38cd94d97f7e54420b5fbf65f5405cf95dd7
blackeagle686 <mathematecs1@gmail.com>
2026-06-04 16:41:20 +0300

    Export BaseReflectorMeta in reflector module

--- phoenix/framework/agent/cognition/reflector/__init__.py
+++ phoenix/framework/agent/cognition/reflector/__init__.py
@@ -1,5 +1,6 @@
 from .base import BaseReflector
 from .reflector import Reflector
+from .schema import BaseReflectorMeta
 
-__all__ = ["BaseReflector", "Reflector"]
+__all__ = ["BaseReflector", "Reflector", "BaseReflectorMeta"]
 
