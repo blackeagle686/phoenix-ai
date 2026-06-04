6b99279ad90603879db3f386d3fcb621456cf655
blackeagle686 <mathematecs1@gmail.com>
2026-06-04 16:37:21 +0300

    Add trailing newline

--- phoenix/framework/agent/cognition/planner/schema.py
+++ phoenix/framework/agent/cognition/planner/schema.py
@@ -117,6 +117,7 @@ class Solution(BaseModel):
     description: str = Field(..., description="solution description")
     solution_type: SolutionType = Field(..., description="Type of solution")
     content: str = Field(..., description="solution content")
+    
 
 class Problems(BaseModel): 
     id: UUID = Field(default_factory=uuid4, description="ID")
