286c1b0b9d95e40a3e4d90aacb21412588ad42cb
blackeagle686 <mathematecs1@gmail.com>
2026-06-04 16:40:30 +0300

    Add BaseReflectorMeta schema

---
+++ phoenix/framework/agent/cognition/reflector/schema.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+from typing import Dict, List, Optional, Union
+from enum import Enum
+from uuid import UUID, uuid4
+
+class BaseReflectorMeta(BaseModel):
+    )
