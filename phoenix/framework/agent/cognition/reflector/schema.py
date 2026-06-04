074bb35468aaf54c2f9716d343eb0a72a5e855ac
blackeagle686 <mathematecs1@gmail.com>
2026-06-04 18:06:04 +0300

    add reflector schema

--- phoenix/framework/agent/cognition/reflector/schema.py
+++ phoenix/framework/agent/cognition/reflector/schema.py
@@ -3,6 +3,8 @@ from typing import Dict, List, Optional, Union
 from enum import Enum
 from uuid import UUID, uuid4
 
+class Refle
+
 class BaseReflectorMeta(BaseModel):
     rateing: int = Field(..., description="rateing of solution")
     feedback: str = Field(..., description="feedback")
