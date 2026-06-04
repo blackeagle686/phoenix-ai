c9cd72b975c0f1bc8cae65f3d3583829168e9930
blackeagle686 <mathematecs1@gmail.com>
2026-06-04 15:52:11 +0300

    Rename BaseTask to BaseTaskOutputSchema

--- phoenix/framework/agent/cognition/planner/schema.py
+++ phoenix/framework/agent/cognition/planner/schema.py
@@ -330,7 +330,8 @@ class BaseTaskInputSchema(BaseModel):
     task_description: str = Field(..., description="Task description")
     task_type: str = Field(..., description="Task type")
 
-class BaseTask
+class BaseTaskOutputSchema(BaseModel): 
+    
 
 class WriteTask(BaseModel):
     language: str = Field(..., description="Programming language of the file to be generated")
