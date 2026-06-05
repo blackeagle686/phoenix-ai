370bcef2b80faa8ca5b51191f4d1bf6d85f2476d
blackeagle686 <mathematecs1@gmail.com>
2026-06-05 12:31:12 +0300

    Auto-commit: 2026-06-05 12:31:12

--- phoenix/framework/agent/cognition/planner/task_creator.py
+++ phoenix/framework/agent/cognition/planner/task_creator.py
@@ -104,8 +104,7 @@ class TaskCreator:
         for problem in self.problems:
             solution = await self.create_solution(problem)
             solutions.append(solution)
-
-        
+            
         return solutions
 
     async def create_task(self, objective: str, user_prompt: str) -> Task:
