2f78bd98c2b89ad807ebf919786c0f9fa57ae619
blackeagle686 <mathematecs1@gmail.com>
2026-06-07 12:01:06 +0300

    Add interfaces barrel export file

---
+++ tests/agent/agent_new_arch/front_test/src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from './key-manager.js';
+export * from './transaction-manager.js';
+export * from './blockchain-provider.js';
+export * from './vault.js';
+export * from './event-emitter.js';
