a9bf1ce889cff44ba7519ca507baaed73df2142b
blackeagle686 <mathematecs1@gmail.com>
2026-06-09 11:42:07 +0300

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ EXTRAS["full"] = list(set(dep for deps in EXTRAS.values() for dep in deps))
 
 setup(
     name="phx-ashborn",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=1.0.0",
