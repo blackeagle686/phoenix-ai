8217cd7b2368fd45811dca0aaec7e1a8faacd8e3
blackeagle686 <mathematecs1@gmail.com>
2026-06-09 11:05:06 +0300

    Add RAG and config exports to framework init

--- phoenix/framework/__init__.py
+++ phoenix/framework/__init__.py
@@ -5,9 +5,13 @@ from .multi_agent.config import MultiAgentConfig, AgentConfig
 from .multi_agent.protocol import AgentMessage, MessageType, Priority
 from .multi_agent.message_bus import MessageBus
 from .multi_agent.state_store import SharedStateStore
+from .rag import RAG, CAG, AgenticRAG, AdaptiveRAG, MultiModalRAG, BaseRAG
+from .rag.config import RAGConfig, CAGConfig, AgenticRAGConfig, MultiModalRAGConfig
 
 __all__ = [
     "ChatBot", "Agent", "MultiAgentManager", "MultiAgentConfig", "AgentConfig",
-    "AgentMessage", "MessageType", "Priority", "MessageBus", "SharedStateStore"
+    "AgentMessage", "MessageType", "Priority", "MessageBus", "SharedStateStore",
+    "RAG", "CAG", "AgenticRAG", "AdaptiveRAG", "MultiModalRAG", "BaseRAG",
+    "RAGConfig", "CAGConfig", "AgenticRAGConfig", "MultiModalRAGConfig",
 ]
 
