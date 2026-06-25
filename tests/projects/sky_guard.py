f0791619fb41d95df6d6613571f904abddc6e071
blackeagle686 <mathematecs1@gmail.com>
2026-06-25 15:10:41 +0300

    Fix sys.path in sky_guard test

--- tests/projects/sky_guard.py
+++ tests/projects/sky_guard.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Ensure SDK is in path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from phoenix.framework.sensorium.core.manager import DeviceManager
 from phoenix.framework.sensorium.devices.simulators import RadarSimulator
